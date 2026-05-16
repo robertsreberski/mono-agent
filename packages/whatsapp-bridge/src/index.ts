f60819ee04f4d5144f659a9f5930bd06adf2eafd
robertsreberski <robert.sreberski@automattic.com>
2026-05-16 10:13:20 +0200

    feat: add whatsapp bridge message handling

--- packages/whatsapp-bridge/src/index.ts
+++ packages/whatsapp-bridge/src/index.ts
@@ -1,1 +1,68 @@
-export {};
+export type {
+  AgentRequest,
+  AgentResponder,
+  AgentResponse,
+  WhatsAppBridgeIgnoredReason,
+  WhatsAppBridgeLogger,
+  WhatsAppBridgeMessages,
+  WhatsAppBridgeOptions,
+  WhatsAppBridgeStreamOptions,
+  WhatsAppGroupTriggerMode,
+  WhatsAppMessageHandlingResult,
+  WhatsAppRequestMetadata,
+  WhatsAppTriggerKind,
+  WhatsAppTriggerOptions,
+} from "./bridge.js";
+export {
+  AgentResponderCancelledError,
+  isAgentResponderCancelledError,
+  WhatsAppBridge,
+} from "./bridge.js";
+
+export type {
+  WhatsAppMessageIgnoredReason,
+  WhatsAppMessageNormalizationResult,
+} from "./message-normalizer.js";
+export { isGroupJid, normalizeWhatsAppMessage } from "./message-normalizer.js";
+
+export type {
+  AgentMessageStream,
+  WhatsAppMessageStreamLogger,
+  WhatsAppMessageStreamOptions,
+} from "./message-stream.js";
+export { splitWhatsAppText, WhatsAppMessageStream } from "./message-stream.js";
+
+export type {
+  AgentRuntimeLike,
+  RuntimeEventLike,
+  RuntimeExecutionMode,
+  RuntimeMessage,
+  RuntimeMessageBuilder,
+  RuntimeModelReference,
+  RuntimeResponderErrorDetails,
+  RuntimeResponderOptions,
+  RuntimeResultLike,
+  RuntimeRunOptions,
+} from "./runtime-responder.js";
+export {
+  assistantTextFromRuntimeEvent,
+  createRuntimeResponder,
+  defaultRuntimeMessages,
+  RuntimeResponderError,
+} from "./runtime-responder.js";
+
+export type {
+  LongLike,
+  WhatsAppChatKind,
+  WhatsAppContextInfoLike,
+  WhatsAppEventEmitterLike,
+  WhatsAppJid,
+  WhatsAppMessageContentLike,
+  WhatsAppMessageKeyLike,
+  WhatsAppRawMessage,
+  WhatsAppSendMessageContent,
+  WhatsAppSendMessageOptions,
+  WhatsAppSentMessage,
+  WhatsAppSocketLike,
+  WhatsAppTextMessage,
+} from "./types.js";
